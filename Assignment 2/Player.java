09c9558e765d62841c398c6a30604c1efcfcf8ae
01piyush99 <65732879+01piyush99@users.noreply.github.com>
2023-09-26 01:17:47 +0530

    Task 4

--- Assignment 2/Player.java
+++ Assignment 2/Player.java
@@ -1,10 +1,16 @@
 public class Player {
     public int numPlayers;
     public Player(int p) {
-        this.numPlayers=p;
+        this.numPlayers = p;
     }
+
     public static void main(String[] args) {
-        Player p=new Player(5);
-        System.out.println("The number players is:"+p.numPlayers);
+        PlayerData data=new PlayerData();
+        PlayerData[] myData=data.getMyData();
+        System.out.println("The number of players is: "+myData.length);
+        System.out.println("And the Data is:");
+        for(PlayerData i : myData) {
+            System.out.println("A:"+i.a+"B:"+i.b+"C:"+i.c);
+        }
     }
 }
