3ec70dc535dd6d8d4be3532caf66343986162483
01piyush99 <65732879+01piyush99@users.noreply.github.com>
2023-09-26 01:53:25 +0530

    Task 7

--- Assignment 2/Player.java
+++ Assignment 2/Player.java
@@ -1,16 +1,29 @@
 public class Player {
     public int numPlayers;
+    public SeLinkList head;
     public Player(int p) {
         this.numPlayers = p;
+        this.head = null; 
+
+    }
+    public void addPlayer(int p1,int p2,int p3){
+
+        SeLinkList LL=new SeLinkList(p1,p2,p3);
+        LL.next=head;
+        head=LL; 
     }
 
     public static void main(String[] args) {
-        PlayerData data=new PlayerData();
-        PlayerData[] myData=data.getMyData();
-        System.out.println("The number of players is: "+myData.length);
+        Player P=new Player(5);
+        P.addPlayer (4, 7, 5);
+        P.addPlayer(3, 6, 14 );
+        P.addPlayer(1, 11, 0 );
+        SeLinkList ptr =P.head;
+        System.out.println("The number of players is:"+P.numPlayers);
         System.out.println("And the Data is:");
-        for(PlayerData i : myData) {
-            System.out.println("A:"+i.a+"B:"+i.b+"C:"+i.c);
+        while(ptr!=null) {
+            System.out.println(ptr.a+" "+ptr.b+" "+ptr.c);
+            ptr= ptr.next;
         }
     }
 }
