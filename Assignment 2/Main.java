1b352f01957844a8fb42476151cb5129d9a3462d
01piyush99 <65732879+01piyush99@users.noreply.github.com>
2023-09-26 15:22:01 +0530

    Task 16

--- Assignment 2/Main.java
+++ Assignment 2/Main.java
@@ -1,7 +1,26 @@
-
 public class Main {
-public static void main(String[] args) {
-    System.out.println("Assignment Task 1");
-}
-    
+    public static void main(String[] args) {
+        System.out.println("Assignment Task 1");
+
+        // Instantiate Player
+        Player player = new Player(0);
+
+        // InitializeList()
+        player.addPlayer(4, 7, 5);
+        player.addPlayer(3, 6, 14);
+        player.addPlayer(1, 11, 0);
+
+        for (int i = 0; i < 3; i++) {
+            // Find Min and Max weights and print out the Max and Min nodes.
+            SeLinkList[] minMaxPair = player.findMinMaxWeight();
+            System.out.println("Min Node: Index=" + minMaxPair[0].index + " a=" + minMaxPair[0].a + " b=" + minMaxPair[0].b + " c=" + minMaxPair[0].c);
+            System.out.println("Max Node: Index=" + minMaxPair[1].index + " a=" + minMaxPair[1].a + " b=" + minMaxPair[1].b + " c=" + minMaxPair[1].c);
+
+            // Remove Max and Min nodes.
+            player.removeNode(minMaxPair[0].index);
+            player.removeNode(minMaxPair[1].index);
+
+            System.out.println("[Done!]");
+        }
+    }
 }
--- Assignment 2/Player.java
+++ Assignment 2/Player.java
@@ -1,68 +1,98 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
+
 public class Player {
     public int numPlayers;
     public SeLinkList head;
-    public Player(int p) {
-        this.numPlayers = p;
-        this.head = null; 
-
+    public Player(int p){
+        this.numPlayers=p;
+        this.head=null;
     }
-    public void addPlayer(int p1,int p2,int p3){
+    public void addPlayer(int p1, int p2, int p3) {
+        SeLinkList LL = new SeLinkList(p1, p2, p3, numPlayers);
+        LL.next = head;
+        head = LL;
+        numPlayers = numPlayers + 1;
+    }
 
-        SeLinkList LL=new SeLinkList(p1,p2,p3,numPlayers);
-        LL.next=head;
-        head=LL; 
-        numPlayers=numPlayers+1;
+    public void removeNode(int i){
+        if(head==null){
+            System.out.println("Empty linked list");
+            return;
+        }
+        if(i==0){
+            numPlayers=numPlayers-1;
+            head=head.next;
+            return;
+        }
+        int curInd = 0;
+        SeLinkList ptr=head;
+        while(curInd<i-1 && ptr!=null){
+            curInd=curInd+1;
+            ptr=ptr.next;
+        }
+        if( ptr.next==null || ptr==null ){
+            System.out.println("Index"+i+" not found.");
+            return;
+        }
+        numPlayers=numPlayers-1;
+        ptr.next=ptr.next.next;
     }
     public SeLinkList[] findMinMaxWeight() {
         if(head==null){
             System.out.println("Empty linked list");
             return null;
         }
-        SeLinkList ptr=head.next,minNode=head,maxNode=head;
+
+        SeLinkList ptr=head.next,maxNode=head,minNode=head;
         int mini=head.a+head.b+head.c;
         int Maxi=mini;
         while(ptr!=null){
             int w=ptr.a+ptr.b+ptr.c;
-            if(w<mini){
-                mini=w;
-                minNode=ptr;
-            }
             if(w>Maxi){
                 Maxi=w;
                 maxNode=ptr;
             }
+            if(w<mini){
+                minNode=ptr;
+                mini=w;
+            }
             ptr=ptr.next;
-        }   
+        }
         System.out.println("Minimum weight is: "+mini);
         System.out.println("Maximum weight is: "+Maxi);
 
-        SeLinkList[] result = {minNode,maxNode};
-        return result;
+        SeLinkList[] res={minNode,maxNode};
+        return res;
     }
+
     public static void main(String[] args) {
-        // System.out.println("Task4");
-        // PlayerData data=new PlayerData();
-        // PlayerData[] myData=data.getMyData();
-        // System.out.println("The number of players is: "+myData.length);
-        // System.out.println("And the Data is:");
-        // for(PlayerData i : myData) {
-        //     System.out.println("A:"+i.a+"B:"+i.b+"C:"+i.c);
-        // }
         System.out.println("Task9");
-        Player P=new Player(5);
-        P.addPlayer (4, 7, 5);
-        P.addPlayer(3,6, 14);
-        P.addPlayer(1, 11, 0);
-        SeLinkList ptr =P.head;
+        Player P=new Player(0);
+        P.addPlayer(4,7,5);
+        P.addPlayer(3,6,14);
+        P.addPlayer(1,1,0);
+        SeLinkList ptr=P.head;
         System.out.println("The number of players is:"+P.numPlayers);
-        while(ptr!=null) {
-            System.out.println("Index is " + ptr.index+", a is "+ptr.a+", b is "+ptr.b+" and c is "+ptr.c);            
-            ptr= ptr.next;
+        while(ptr!=null){
+            System.out.println("Index is "+ptr.index+", a is "+ptr.a+", b is "+ptr.b+" and c is "+ptr.c);            
+            ptr=ptr.next;
         }
         SeLinkList[] minMaxPair=P.findMinMaxWeight();
+        System.out.println("Max Node: Index="+minMaxPair[1].index+" a="+minMaxPair[1].a+" b="+minMaxPair[1].b+" c=" + minMaxPair[1].c);
         System.out.println("Min Node: Index="+minMaxPair[0].index+" a="+minMaxPair[0].a+" b="+minMaxPair[0].b+" c=" + minMaxPair[0].c);
-        System.out.println("Max Node: Index="+minMaxPair[1].index+" a="+minMaxPair[1].a+" b="+minMaxPair[1].b+" c=" +minMaxPair[1].c);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter index to remove");
+        int index=sc.nextInt();
+        if(index>P.numPlayers){
+            System.out.println("Index not found");
+            return;
+        }
+        P.removeNode(index);
+        ptr=P.head;
+        System.out.println("After removing index "+index+", there are: "+P.numPlayers+" players overall");
+        while(ptr!=null){
+            System.out.println("a is "+ptr.a+", b is "+ptr.b+" and c is "+ptr.c);
+            ptr=ptr.next;
+        }
     }
 }
